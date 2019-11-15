65d0772bfca7a1a4c45badd1ee2d8b387b8131ba
vantalon <martin.vantalon@epitech.eu>
2019-11-15 20:53:10 +0100

    added the widgets linkers

---
+++ dashboard/src/main/java/com/dashboard/WidgetsController/WidgetsController.java
@@ -0,0 +1,38 @@
+package dashboard.WidgetsController;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.Authentication;
+import java.util.Map;
+import dashboard.LinkWidgetsUser.LinkWidgetsUserRepository;
+import dashboard.LinkWidgetsUser.LinkWidgetsUserTable;
+import dashboard.Widgets.WidgetsTable;
+import dashboard.Widgets.WidgetsRepository;
+
+import java.net.*;
+import java.io.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import java.text.ParseException;
+import org.json.simple.parser.JSONParser;
+import dashboard.WidgetsController.WidgetsController;
+
+
+public interface WidgetsController {
+
+  public String getData(String input)throws IOException, ServletException, IOException, InterruptedException, ExecutionException;
+}
