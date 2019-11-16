7cb2dbc352634d797b85595d76ff85855aeb9e2d
vantalon <martin.vantalon@epitech.eu>
2019-11-16 10:39:08 +0100

    add the hellos elements, wait your login authentification

---
+++ dashboard/src/main/java/com/dashboard/Hello.java
@@ -0,0 +1,19 @@
+package dashboard;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Map;
+import java.util.HashMap;
+/*
+@RestController
+public class Hello {
+
+    @RequestMapping("/home")
+    public ModelAndView index() {
+        Map<String, String> tmp = new HashMap<>();
+		    return new ModelAndView("allEmployees.html");
+    }
+
+}
+*/
